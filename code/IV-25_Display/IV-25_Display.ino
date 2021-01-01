7e4b2ac53d70a06f7fb811a1ee13b878a0804eb5
Martin Schörner <fan4tix@freenet.de>
2021-01-01 23:57:27 +0100

    removed "borrowed" header from hello world sketch

--- code/IV-25_Display/IV-25_Display.ino
+++ code/IV-25_Display/IV-25_Display.ino
@@ -1,13 +1,3 @@
-
-//**************************************************************//
-//  Name    : shiftOutCode, Hello World
-//  Author  : Carlyn Maw,Tom Igoe, David A. Mellis
-//  Date    : 25 Oct, 2006
-//  Modified: 23 Mar 2010
-//  Version : 2.0
-//  Notes   : Code for using a 74HC595 Shift Register           //
-//          : to count from 0 to 255
-//****************************************************************
 #include "display.h"
 
 int latchPin = 13; //Pin connected to ST_CP of 74HC595
