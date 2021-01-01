ac1b6194eed8b5409c18e9e61392a919610d974d
Martin Schörner <fan4tix@freenet.de>
2021-01-01 23:55:26 +0100

    Decluttered hello world sketch

--- code/IV-25_Display/IV-25_Display.ino
+++ code/IV-25_Display/IV-25_Display.ino
@@ -9,21 +9,18 @@
 //          : to count from 0 to 255
 //****************************************************************
 #include "display.h"
-//Pin connected to ST_CP of 74HC595
-int latchPin = 13;
-//Pin connected to SH_CP of 74HC595
-int clockPin = 16;
-////Pin connected to DS of 74HC595
-int dataPin = 14;
- char buf[100];
- int i = 1330;
+
+int latchPin = 13; //Pin connected to ST_CP of 74HC595
+int clockPin = 16; //Pin connected to SH_CP of 74HC595
+int dataPin = 14; //Pin connected to DS of 74HC595
+
 IV25Display dsp( 6 * 8, latchPin, dataPin, clockPin);
+
 void setup() {
-  Serial.begin(115200);
+
 }
 
 void loop() {
   dsp.print("Hello World!")
   delay(1000);
-
 }
